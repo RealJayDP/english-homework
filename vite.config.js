44f8d8a93785cf01b3d0638f85a122884bfd8926
RealJayDP <johan.brannstrom@gmail.com>
2026-02-05 20:32:13 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/klara-english-homework/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/english-homework/' : '/',
 })
